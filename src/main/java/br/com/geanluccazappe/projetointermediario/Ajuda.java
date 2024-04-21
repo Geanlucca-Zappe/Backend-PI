93ce04d591d7c0ce5c2a274e66334c2cadfbca7f
Geanlucca-Zappe <gzubialli@gmail.com>
2024-04-21 19:01:36 -0300

    Projeto

---
+++ src/main/java/br/com/geanluccazappe/projetointermediario/Ajuda.java
@@ -0,0 +1,22 @@
+package br.com.geanluccazappe.projetointermediario;
+
+public class Ajuda {
+    private String estudante;
+    private String projeto;
+
+    public String getEstudante() {
+        return estudante;
+    }
+
+    public void setEstudante(String estudante) {
+        this.estudante = estudante;
+    }
+
+    public String getProjeto() {
+        return projeto;
+    }
+
+    public void setProjeto(String projeto) {
+        this.projeto = projeto;
+    }
+}
